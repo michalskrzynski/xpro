005881d979fd64007acd500bd48fef0db6b4cf9e
michalskrzynski <michal.skrzynski@gmail.com>
2022-09-16 13:02:24 +0200

    change ball animation to object oriented

--- module2/simulating-ball-movement/ball-solution.js
+++ module2/simulating-ball-movement/ball-solution.js
@@ -1,30 +1,86 @@
-//Set global variable that would contain the position, velocity and the html element "ball"
-var positionX = 0;
-var positionY = 0;
-var velocityX = 50;
-var velocityY = 20;
-var reverseX = -1;
-var reverseY = -1;
-var ball = document.getElementById("ball");
+var zIndex = 1;
 
-//write a function that can change the position of the html element "ball"
-function moveBall() {
-  // two x-axis coordinates
-  var Xmin = 0;
-  var Xmax = 300;
+const ballFactory = (radius, color=null, x=null, y=null) => {
+  return {
+    radius: radius,
+    color: color == null ? "black" : color,
 
-  // two y-axis coordinates
-  var Ymin = 0;
-  var Ymax = 300;
+    xMin: 0,
+    xMax: 300,
 
-  positionX += velocityX*reverseX;
-  ball.style.left = positionX + "px";
+    yMin: 0,
+    yMax: 300,
 
-  positionY += velocityY*reverseY;
-  ball.style.top = positionY + "px";
+    positionX: null,
+    positionY: null,
+    
+    velocityX: 50,
+    velocityY: 20,
 
-  if ( positionX > Xmax || positionX < Xmin ) reverseX *= -1;
-  if ( positionY > Ymax || positionY < Ymin ) reverseY *= -1;
+    reverseX: -1,
+    reverseY: -1,
+
+    div: null,
+
+    draw() {
+      this.positionX = x == null ? Math.floor( Math.random() * this.xMax ) : x,
+      this.positionY = y == null ? Math.floor( Math.random() * this.yMax ) : y,
+
+      this.div = document.createElement('div');
+      let body = document.querySelector( "body" ).appendChild( this.div );
+
+      this.div.style.position = "absolute";
+      this.div.style.zIndex = zIndex++;
+      this.div.style.left = this.positionX + "px";
+      this.div.style.top = this.positionY + "px";
+      this.div.style.width = this.radius*2 + "px";
+      this.div.style.height = this.radius*2 + "px";
+      this.div.style.borderRadius = "50%";
+      this.div.style.background = this.color;
+      
+      console.log(this.div.style.left, this.div.style.top);
+    },
+
+    moveBall() {
+      if( this.div === null ) return;
+    
+      this.positionX += this.velocityX*this.reverseX;
+      this.div.style.left = this.positionX + "px";
+    
+      this.positionY += this.velocityY*this.reverseY;
+      this.div.style.top = this.positionY + "px";
+    
+      if ( this.positionX > this.xMax || this.positionX < this.xMin ) this.reverseX *= -1;
+      if ( this.positionY > this.yMax || this.positionY < this.yMin ) this.reverseY *= -1;
+    },
+
+    startMoving(interval) {
+      if( this.div === null ) return;
+
+
+      var _this = this;
+      setInterval(function() {
+          _this.moveBall();
+      }, interval); 
+    }
+  }
+
 }
 
-setInterval(moveBall, 100);
+
+  let b1 = ballFactory(20, "red");
+  b1.draw();
+  b1.startMoving(100);
+
+  let b2 = ballFactory(30, "green");
+  b2.draw();
+  b2.startMoving(80);
+
+  let b3 = ballFactory(50, "blue");
+  b3.draw();
+  b3.startMoving(140);
+
+  let b4 = ballFactory(10, "black");
+  b4.draw();
+  b4.startMoving(200);
+
